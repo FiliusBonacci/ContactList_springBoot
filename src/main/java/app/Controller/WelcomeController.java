015bba1c3f44119e216e96ec9bbcb148e96be453
Paweł Jadanowski <pjadanowski>
2018-07-05 16:31:35 +0200

    updating person with axios

--- src/main/java/app/Controller/WelcomeController.java
+++ src/main/java/app/Controller/WelcomeController.java
@@ -2,6 +2,9 @@ package app.Controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Map;
@@ -19,6 +22,12 @@ public class WelcomeController {
     @RequestMapping("/addNewPerson")
     public String addNewPerson() {
         return "addPerson";
+    }
+
+    @RequestMapping("/editPerson/{id}")
+    public String editPerson(@PathVariable String id, Model model) {
+        model.addAttribute("id", id);
+        return "editPerson";
     }
 
 }
